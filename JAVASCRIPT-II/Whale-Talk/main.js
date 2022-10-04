612fc3df8664cee303d1007075d297bafed9a281
Punvireakroth <vireakrothpun@gmail.com>
2022-10-04 22:02:32 +0700

    final result

--- JAVASCRIPT-II/Whale-Talk/main.js
+++ JAVASCRIPT-II/Whale-Talk/main.js
@@ -1,15 +1,20 @@
-let input = "Hi you";
+let input = "i am vireakroth";
 
 vowels = ["a", "e", "i", "o", "u"];
 
 resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
+  if (input[i] === "e") {
+    resultArray.push(input[i]);
+  } else if (input[i] === "u") {
+    resultArray.push(input[i]);
+  }
   for (let j = 0; j < vowels.length; j++) {
     if (input[i] === vowels[j]) {
-      console.log(input[i]);
-    } else {
-      console.log("no");
+      resultArray.push(input[i]);
     }
   }
 }
+let resultString = resultArray.join("");
+console.log(resultString.toUpperCase());
