218899e140d820b8f90879a0228ecbb5f9c19a26
Punvireakroth <vireakrothpun@gmail.com>
2022-10-13 23:47:07 +0700

    finish challenge

--- JAVASCRIPT-II/Meal-Maker/main.js
+++ JAVASCRIPT-II/Meal-Maker/main.js
@@ -3,8 +3,27 @@ const menu = {
   _price: 0,
 
   set meal(mealToCheck) {
-    if (mealToCheck === "string") {
-      mealToCheck = this._meal;
+    if (typeof mealToCheck === "string") {
+      this._meal = mealToCheck;
+    }
+  },
+
+  set price(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      this._price = priceToCheck;
+    }
+  },
+
+  get todaysSpecial() {
+    if (this._meal && this._price) {
+      return `Today's Meal is ${this._meal} for ${this._price}!`;
+    } else {
+      return `Meal or price not set correctly`;
     }
   },
 };
+
+menu.meal = "pizza";
+menu.price = 20;
+
+console.log(menu.todaysSpecial);
