5aecc2c19b8b459527ed0cb68179ac9471b25fdd
Punvireakroth <vireakrothpun@gmail.com>
2022-10-24 21:54:24 +0700

    initial commit

---
+++ JAVASCRIPT-II/Practice/greetAliens.js
@@ -0,0 +1,10 @@
+let arr = [];
+const greetAliens = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`);
+  }
+};
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
