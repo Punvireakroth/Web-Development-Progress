aa48feb80d3907a858f3752e81e506619dccedc6
Punvireakroth <vireakrothpun@gmail.com>
2022-10-26 21:43:29 +0700

    initial commit

--- JAVASCRIPT-II/Practice/justCoolStuff.js
+++ JAVASCRIPT-II/Practice/justCoolStuff.js
@@ -1,18 +1,10 @@
-// Write your code here:
-
 const justCoolStuff = (arr1, arr2) => {
-  arr1.forEach((item1) => {
-    arr2.include(item1);
-    console.log(arr2);
-  });
+  let intersect = arr1.filter((value) => arr2.includes(value));
+  return intersect;
 };
-
-// Feel free to uncomment the code below to test your function
 
 const coolStuff = ["gameboys", "skateboards", "backwards hats", "fruit-by-the-foot", "pogs", "my room", "temporary tattoos"];
 
 const myStuff = ["rules", "fruit-by-the-foot", "wedgies", "sweaters", "skateboards", "family-night", "my room", "braces", "the information superhighway"];
 
-// console.log(justCoolStuff(myStuff, coolStuff));
-justCoolStuff(myStuff, coolStuff);
-// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+console.log(justCoolStuff(myStuff, coolStuff));
