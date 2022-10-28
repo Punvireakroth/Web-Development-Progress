2b6b6a5dacbb15d49a6ba0881fa350cea571ef09
Punvireakroth <vireakrothpun@gmail.com>
2022-10-28 23:15:19 +0700

    initial commit

---
+++ JAVASCRIPT-II/Practice/sortSpeciesByTeeth.js
@@ -0,0 +1,22 @@
+const speciesArray = [
+  { speciesName: "shark", numTeeth: 50 },
+  { speciesName: "dog", numTeeth: 42 },
+  { speciesName: "alligator", numTeeth: 80 },
+  { speciesName: "human", numTeeth: 32 },
+];
+
+// Write your code here:
+
+const sortSpeciesByTeeth = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    return arr[i];
+  }
+};
+
+console.log(sortSpeciesByTeeth(speciesArray));
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
