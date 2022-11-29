1b5859722543e75c0767f2723a42f643b4ebe734
Punvireakroth <vireakrothpun@gmail.com>
2022-11-29 10:01:39 +0700

    rewrite app in VueJs

--- VueJs/firstVueApp/app.js
+++ VueJs/firstVueApp/app.js
@@ -1,14 +1,28 @@
-const buttonEl = document.querySelector("button");
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
+// const buttonEl = document.querySelector("button");
 
-const inputEl = document.querySelector("input");
+// const inputEl = document.querySelector("input");
 
-const listel = document.querySelector("ul");
+// const listel = document.querySelector("ul");
 
-const addGoal = () => {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement("li");
-  listItemEl.textContent = enteredValue;
-  listel.appendChild(listItemEl);
-  inputEl.value = "";
-};
-buttonEl.addEventListener("click", addGoal);
+// const addGoal = () => {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = enteredValue;
+//   listel.appendChild(listItemEl);
+//   inputEl.value = "";
+// };
+// buttonEl.addEventListener("click", addGoal);
