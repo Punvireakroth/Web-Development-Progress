190eb194c4604e99d3a2e6c6301485650d12fca1
Punvireakroth <vireakrothpun@gmail.com>
2023-05-13 17:35:02 +0700

    finish ✅

--- MERN_Stack/Markdown_Blog/server.js
+++ MERN_Stack/Markdown_Blog/server.js
@@ -1,33 +1,33 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const articleRouter = require("./routes/articles.js");
-
+const Article = require("./models/article.js");
+const methodOverride = require("method-override");
 const app = express();
+
+// connect database
+mongoose
+  .connect(
+    "mongodb+srv://vireakrothpun:lwDPSF0Xmm4HNOvK@cluster0.il41wtj.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
 
-app.use("/articles", articleRouter);
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Go Go real madrid",
-      createdAt: new Date(),
-      description: "I am a Real Madrid fan",
-    },
-    {
-      title: "Go Go Cambodia",
-      createdAt: new Date(),
-      description: "I am a Real Madrid fan",
-    },
-    {
-      title: "Go Go Brighton",
-      createdAt: new Date(),
-      description: "I am a Real Madrid fan",
-    },
-  ];
-  res.render("index", { articles: articles });
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter);
 
 app.listen(5000, () => {
   console.log("Server running on PORT: 5000");
